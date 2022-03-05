5652b9a15cdd1d68924c3c55b683ec0421d7bbe0
_Nomad_ <62549015+dralperkorkmaz@users.noreply.github.com>
2022-03-05 23:58:35 +0100

    list methods

--- jsonPack.py
+++ jsonPack.py
@@ -5,7 +5,7 @@ import json
 # json.dump(j,f) -> write JSON object to a file
 # json.dumps(f) -> output JSON object as string
 # some JSON:
-json_file=open("../data/json1.txt", "r", encoding="utf-8")
+json_file=open("data/json1.txt", "r", encoding="utf-8")
 text_file=json.load(json_file)
 json_file.close()
 print(text_file)
@@ -22,6 +22,6 @@ movie1["composer"]="Mike Hammer"
 movie1["actors"]=["Bruce Willis","Samantha Path", "Eva Gerzegova"]
 movie1["rate"]=8
 movie1["Company"]="Warner Bros"
-file2=open("../data/json2.txt","w",encoding="utf-8")
+file2=open("data/json2.txt", "w", encoding="utf-8")
 json.dump(movie1,file2,ensure_ascii=False)
 file2.close()
